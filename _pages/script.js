d88786844d8bffe114364b82bf52c811288dd008
NajaAFonseca <NajaAFonseca@users.noreply.github.com>
2023-05-31 06:21:14 +0000

    deploy: 74b646fd0e38891b157f3a378660acc23fea6fc2

--- _pages/script.js
+++ _pages/script.js
@@ -8,35 +8,35 @@ const apiHost = 'spotify23.p.rapidapi.com';
 searchButton.addEventListener('click', searchArtists);
 
 async function searchArtists() {
-    const artistName = searchInput.value.trim();
-    if (artistName === '') {
-        alert('Please enter an artist name');
-        return;
-    }
+	const artistName = searchInput.value.trim();
+	if (artistName === '') {
+		alert('Please enter an artist name');
+		return;
+	}
 
-    const url = `https://${apiHost}/artists/?ids=${artistName}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': apiHost
-        }
-    };
+	const url = `https://${apiHost}/search/?q=${artistName}&type=artist`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': apiKey,
+			'X-RapidAPI-Host': apiHost
+		}
+	};
 
-    try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        displayResults(data);
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		const response = await fetch(url, options);
+		const data = await response.json();
+		displayResults(data);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 function displayResults(data) {
 	searchResults.innerHTML = '';
 
-	if (data && data.length > 0) {
-		data.forEach(artist => {
+	if (data && data.artists && data.artists.items.length > 0) {
+		data.artists.items.forEach(artist => {
 			const artistName = artist.name;
 			const artistImage = artist.image;
 			const artistElement = document.createElement('div');
@@ -48,3 +48,4 @@ function displayResults(data) {
 		searchResults.innerHTML = 'No artists found';
 	}
 }
+
